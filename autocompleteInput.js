e27e5a6e77976897ac2fdf6318e4d4df5954b761
Drew <stagedown@gmail.com>
2014-08-29 15:25:00 +0300

    Update autocompleteInput.js

--- autocompleteInput.js
+++ autocompleteInput.js
@@ -2,15 +2,25 @@ angular.directive('autocompleteInput', function () {
   
    return {
   
+    //we've used the require property to tell Angular that we want
+    //access to the controller instance on the ngModel directive
+    //Because we've required the ngModel
+    //controller, we also have to declare the directive that provides that controller, 
+    //otherwise Angular will throw an error, which means that our input element needs to look like:
+    //<input ng-model="data.property" autocomplete-input />
      require : 'ngModel',
     
      link : function ($scope, $element, $attrs, ngModel) {
-    
-       ngModel.$render = function () {
+        //The ngModel controller provides multiple methods and properties, but for our 
+        //purposes we only need two, $render and $setViewValue().
+        
+        //Thus, once we assign our custom function
+        //to the $render key, any time the data changes, we can update the input value appropriately.
+      ngModel.$render = function () {
       
-       $element.val(ngModel.$viewValue || '');
+        $element.val(ngModel.$viewValue || '');
       
-     };
+      };
     
      $element.autocomplete({
     
@@ -20,6 +30,11 @@ angular.directive('autocompleteInput', function () {
       
        $scope.$apply(function() {
       
+      
+
+        //$setViewValue works in the opposite direction, so when the user
+        //does something that should change the value, we can tell ngModel what the new 
+        //value is and it will update the internal data model.
        ngModel.$setViewValue(ui.item.value);
       
      });
